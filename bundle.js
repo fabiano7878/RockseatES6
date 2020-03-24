c0eb3917aecfd0f5733aaee8e708f7c9c5667fbf
Fabiano Dos Santos Silva <fabsilva@scopus.com.br>
2020-03-24 17:05:48 -0300

    continuação aulas

--- bundle.js
+++ bundle.js
@@ -1,22 +1,113 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./main.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./funcoes.js":
+/*!********************!*\
+  !*** ./funcoes.js ***!
+  \********************/
+/*! exports provided: soma */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
 "use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"soma\", function() { return soma; });\nfunction soma(a, b) {\n  return a + b;\n}\n\n//# sourceURL=webpack:///./funcoes.js?");
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/***/ }),
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+/***/ "./main.js":
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _funcoes__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./funcoes */ \"./funcoes.js\");\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\nfunction _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }\n\nfunction _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }\n\n/**\r\n * Aula Instalando node e yarn, Aula configurando Babel.\r\n * Configuramos o NodeJS, o Yarn e o Babel.\r\n * Assim como executamos o ambiente de desenvolvimento e instalamos alguymas dependencias que o Babel precisa.\r\n * criamos o código abaixo, demostrando como o babel consegue interpretar o ES6 com a sintaxe de orientação a objetos.\r\n * veja a sintaxe, chamando uma classe e um metodo, o babel criou um arquivo chamado bundle e converte o código para \r\n * uma sintaxe que todos os browsers conseguem entender. o alert foi de teste, ele mostra que algumas coisas em java script\r\n * continuarão funcionando.\r\n */\nalert(\"Teste!\");\n\nvar Teste = /*#__PURE__*/function () {\n  function Teste() {\n    _classCallCheck(this, Teste);\n  }\n\n  _createClass(Teste, [{\n    key: \"metodo\",\n    value: function metodo() {}\n  }]);\n\n  return Teste;\n}();\n/** \r\n * Aula Classes, exemplifica um pouco mais sobre o conceito de orientação objeto na implementação com o ES6\r\n*/\n\n/**\r\n * Aula Short Sintaxe, aprendemos configurar o ambiente com o webpack e trocar o babel de execução pelo web pack.\r\n * depois escrevemos o código abaixo mais inxuto, criamos o arquivo funcoes.js com a função soma() e\r\n * importamos aqui no mani.js passando os parametros e xecutamos.\r\n */\n\n\n\nconsole.log(Object(_funcoes__WEBPACK_IMPORTED_MODULE_0__[\"soma\"])(3, 2));\n\n//# sourceURL=webpack:///./main.js?");
 
-alert("Teste!");
+/***/ })
 
-var Teste = /*#__PURE__*/function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
-  }
-
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }]);
-
-  return Teste;
-}();
+/******/ });
--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+/**
+ * Aula Instalando node e yarn, Aula configurando Babel.
+ * Configuramos o NodeJS, o Yarn e o Babel.
+ * Assim como executamos o ambiente de desenvolvimento e instalamos alguymas dependencias que o Babel precisa.
+ * criamos o código abaixo, demostrando como o babel consegue interpretar o ES6 com a sintaxe de orientação a objetos.
+ * veja a sintaxe, chamando uma classe e um metodo, o babel criou um arquivo chamado bundle e converte o código para 
+ * uma sintaxe que todos os browsers conseguem entender. o alert foi de teste, ele mostra que algumas coisas em java script
+ * continuarão funcionando.
+ */
 alert("Teste!");
 
 class Teste{
@@ -6,3 +15,20 @@ class Teste{
         
     }
 }
+
+
+/** 
+ * Aula Classes, exemplifica um pouco mais sobre o conceito de orientação objeto na implementação com o ES6
+*/
+
+
+
+
+
+/**
+ * Aula Short Sintaxe, aprendemos configurar o ambiente com o webpack e trocar o babel de execução pelo web pack.
+ * depois escrevemos o código abaixo mais inxuto, criamos o arquivo funcoes.js com a função soma() e
+ * importamos aqui no mani.js passando os parametros e xecutamos.
+ */
+import {soma} from './funcoes';
+console.log(soma(3,2));
